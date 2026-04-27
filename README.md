64155ff8b2c9fb31e58f4ff936261910fd5eb6e0
rickorme <rick.orme@gmail.com>
2026-04-27 11:14:38 +0200

    Fixed test_sprint_planning.py & updated readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # tapht25d-03_07_playwright
+
+## Regex exercises
+See ```regex.md```
+
+## User Stories and Test scenarios
+See ```user-stories.md```
+
+## e2e tests
+run with ```pytest```
--- tests/e2e/test_sprint_planning.py
+++ tests/e2e/test_sprint_planning.py
@@ -8,12 +8,19 @@ def test_can_view_sprint_planning(page: Page):
     # navigate to the sprint planning page
     page.goto(url, timeout=2000)
 
-    # Find & click the button with the test_id "tn-first"
+    # Find & click the button with the test_id "language-en"
+    engish_button = page.get_by_test_id("language-en")
+    expect(engish_button).to_be_visible()
+    engish_button.click()
+
+    # Find & click the button with the test_id "btn-first"
     first_button = page.get_by_test_id("btn-first")
     expect(first_button).to_be_visible()
+    first_button.click()
 
     # click the button containing the text "Sprint planning"
-    planning_button = page.get_by_role("button").get_by_text(re.compile("Sprint planning"))
+    # planning_button = page.get_by_role("button").get_by_text(re.compile("Sprint planning"))
+    planning_button = page.locator("button").filter(has_text="Sprint planning")
     expect(planning_button).to_be_visible()
     planning_button.click()
 
